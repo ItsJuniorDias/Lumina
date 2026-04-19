745c3476eee03a3d7edf6c3377af6bcdaea1f6e6
ItsJuniorDias <reabilitado97@gmail.com>
2026-04-19 16:30:51 -0300

    chore: add bottom tabs liquid glass

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,75 +1,17 @@
-import React from "react";
-import { Tabs } from "expo-router";
-import { BlurView } from "expo-blur";
-import { StyleSheet, Platform } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { NativeTabs, Label, Icon } from "expo-router/unstable-native-tabs";
 
-export default function AuthLayout() {
+export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: styles.tabBar,
-        // Fundo da Tab Bar usando Glassmorphism
-        tabBarBackground: () => (
-          <BlurView
-            intensity={80}
-            tint="dark"
-            style={StyleSheet.absoluteFill}
-          />
-        ),
-        tabBarActiveTintColor: "#ffffff",
-        tabBarInactiveTintColor: "rgba(255, 255, 255, 0.4)",
-        tabBarShowLabel: true,
-        tabBarLabelStyle: styles.tabBarLabel,
-      }}
-    >
-      {/* Tela do Dashboard (A sua tela principal) */}
-      <Tabs.Screen
-        name="dashboard"
-        options={{
-          title: "Carteira",
-          tabBarIcon: ({ color, size, focused }) => (
-            <Ionicons
-              name={focused ? "wallet" : "wallet-outline"}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
+    <NativeTabs>
+      <NativeTabs.Trigger name="dashboard">
+        <Label>Home</Label>
+        <Icon sf="house.fill" md="home" selectedColor={"#5856D6"} />
+      </NativeTabs.Trigger>
 
-      {/* Tela de Ajustes */}
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Ajustes",
-          tabBarIcon: ({ color, size, focused }) => (
-            <Ionicons
-              name={focused ? "settings" : "settings-outline"}
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      <NativeTabs.Trigger name="settings">
+        <Icon sf="gear" md="settings" selectedColor={"#5856D6"} />
+        <Label>Settings</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
-
-const styles = StyleSheet.create({
-  tabBar: {
-    position: "absolute",
-    borderTopWidth: StyleSheet.hairlineWidth, // Borda ultra fina
-    borderTopColor: "rgba(255, 255, 255, 0.1)",
-    elevation: 0,
-    backgroundColor: "transparent",
-    height: Platform.OS === "ios" ? 88 : 70, // Mais alto no iOS para a Safe Area
-    paddingBottom: Platform.OS === "ios" ? 28 : 10,
-  },
-  tabBarLabel: {
-    fontSize: 11,
-    fontWeight: "500",
-    marginTop: 2,
-  },
-});
